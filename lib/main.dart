d117449c749851c7f50ccc2f08822f11e10458cd
Masato Kikukawa <60773422+kikukawa9@users.noreply.github.com>
2023-03-11 22:20:38 +0900

    new demo app

--- lib/main.dart
+++ lib/main.dart
@@ -20,125 +20,180 @@
 // }
 
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget titleSection = Container(
+//       padding: const EdgeInsets.all(32),
+//       child: Row(
+//         children: [
+//           Expanded(
+//             /*1*/
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 /*2*/
+//                 Container(
+//                   padding: const EdgeInsets.only(bottom: 8),
+//                   child: const Text(
+//                     'Oeschinen Lake Campground',
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//                 Text(
+//                   'Kandersteg, Switzerland',
+//                   style: TextStyle(
+//                     color: Colors.grey[500],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           /*3*/
+//           Icon(
+//             Icons.star,
+//             color: Colors.red[500],
+//           ),
+//           const Text('41'),
+//         ],
+//       ),
+//     );
+
+//     Color color = Theme.of(context).primaryColor;
+
+//     Widget buttonSection = Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//       children: [
+//         _buildButtonColumn(color, Icons.call, 'CALL!'),
+//         _buildButtonColumn(color, Icons.near_me, 'ROUTE!'),
+//         _buildButtonColumn(color, Icons.share, 'SHARE!'),
+//       ],
+//     );
+
+//     Widget textSection = const Padding(
+//       padding: EdgeInsets.all(32),
+//       child: SelectableText(
+//         'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+//         'Alps. Situated 1,578 meters above sea level, it is one of the '
+//         'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+//         'half-hour walk through pastures and pine forest, leads you to the '
+//         'lake, which warms to 20 degrees Celsius in the summer. Activities '
+//         'enjoyed here include rowing, and riding the summer toboggan run.',
+//         // softWrap: true,
+//       ),
+//     );
+
+//     return MaterialApp(
+//       title: 'Flutter layout demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const SelectableText('Flutter layout demo'),
+//         ),
+//         // body: const Center(
+//         //   child: Text('Hello World'),
+//         // ),
+//         body: Column(
+//           children: [
+//             Image.network('https://fordelivery.blob.core.windows.net/democon2/a.jpg',
+//               width: 600,
+//               height: 200,
+//               fit: BoxFit.cover,            
+//             ),
+//             // Image.asset(
+//             //   'images/lake.jpg',
+//             //   width: 600,
+//             //   height: 240,
+//             //   fit: BoxFit.cover,
+//             // ),
+//             titleSection,
+//             buttonSection,
+//             textSection],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Column _buildButtonColumn(Color color, IconData icon, String label) {
+//     return Column(
+//       mainAxisSize: MainAxisSize.min,
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Icon(icon, color: color),
+//         Container(
+//           margin: const EdgeInsets.only(top: 8),
+//           child: SelectableText(
+//             label,
+//             style: TextStyle(
+//               fontSize: 12,
+//               fontWeight: FontWeight.w400,
+//               color: color,
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
-        ],
-      ),
-    );
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL!'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE!'),
-        _buildButtonColumn(color, Icons.share, 'SHARE!'),
-      ],
-    );
-
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: SelectableText(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        // softWrap: true,
-      ),
-    );
-
     return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const SelectableText('Flutter layout demo'),
-        ),
-        // body: const Center(
-        //   child: Text('Hello World'),
-        // ),
-        body: Column(
-          children: [
-            Image.network('https://fordelivery.blob.core.windows.net/democon2/a.jpg',
-              width: 600,
-              height: 200,
-              fit: BoxFit.cover,            
-            ),
-            // Image.asset(
-            //   'images/lake.jpg',
-            //   width: 600,
-            //   height: 240,
-            //   fit: BoxFit.cover,
-            // ),
-            titleSection,
-            buttonSection,
-            textSection],
-        ),
+      title: 'Startup Name Generator',
+      home: RandomWords()
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {  return Scaffold(
+    appBar: AppBar(
+      title: Text('Startup Name Generator'),
+    ),
+    body: _buildSuggestions(),
+  );
+  }
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
+}
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: SelectableText(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
