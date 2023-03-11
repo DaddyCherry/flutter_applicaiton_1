59e3f5cc2aafcf86e53cac3af6ced53e7167903e
Masato Kikukawa <60773422+kikukawa9@users.noreply.github.com>
2023-03-11 15:27:10 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,83 @@
+// import 'package:flutter/material.dart';
+
+// import 'src/app.dart';
+// import 'src/settings/settings_controller.dart';
+// import 'src/settings/settings_service.dart';
+
+// void main() async {
+//   // Set up the SettingsController, which will glue user settings to multiple
+//   // Flutter Widgets.
+//   final settingsController = SettingsController(SettingsService());
+
+//   // Load the user's preferred theme while the splash screen is displayed.
+//   // This prevents a sudden theme change when the app is first displayed.
+//   await settingsController.loadSettings();
+
+//   // Run the app and pass in the SettingsController. The app listens to the
+//   // SettingsController for changes, then passes it further down to the
+//   // SettingsView
+//   runApp(MyApp(settingsController: settingsController));
+// }
+
+
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+void main() => runApp(const MyApp());
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+        // body: Column(children: [titleSection,titleSection,],
+      ),
+    );
+  }
 }
