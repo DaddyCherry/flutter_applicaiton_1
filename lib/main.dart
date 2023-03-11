d7cad6c5625e132f7a66a8b69d15c7584697de12
Masato Kikukawa <kikukawa9@gmail.com>
2023-03-12 00:30:17 +0900

    revise for warning

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@
 //   runApp(MyApp(settingsController: settingsController));
 // }
 
-
 // import 'package:flutter/material.dart';
 
 // void main() => runApp(const MyApp());
@@ -105,7 +104,7 @@
 //             Image.network('https://fordelivery.blob.core.windows.net/democon2/a.jpg',
 //               width: 600,
 //               height: 200,
-//               fit: BoxFit.cover,            
+//               fit: BoxFit.cover,
 //             ),
 //             // Image.asset(
 //             //   'images/lake.jpg',
@@ -146,35 +145,41 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords()
-    );
+    return const MaterialApp(
+        title: 'Startup Name Generator', home: RandomWords());
   }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
-  Widget build(BuildContext context) {  return Scaffold(
-    appBar: AppBar(
-      title: Text('Startup Name Generator'),
-    ),
-    body: _buildSuggestions(),
-  );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator!'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
+
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
+          if (i.isOdd) return const Divider();
 
           final index = i ~/ 2;
           if (index >= _suggestions.length) {
@@ -183,6 +188,7 @@ class RandomWordsState extends State<RandomWords> {
           return _buildRow(_suggestions[index]);
         });
   }
+
   Widget _buildRow(WordPair pair) {
     return ListTile(
       title: Text(
@@ -191,9 +197,4 @@ class RandomWordsState extends State<RandomWords> {
       ),
     );
   }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
 }
